e202a757f70448e817f50554c017d0c05b5274ac
Qiping-Wei <isctae@gmail.com>
2023-10-08 20:43:40 -0500

    add two contracts as test contracts

---
+++ tests/testdata/input_contracts/Crowdsale.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.25;
+  contract Crowdsale {
+  uint256 goal=100000*(10**18);
+  uint256 phase=0;
+  // 0: Active , 1: Success , 2: Refund
+  uint256 raised;
+  uint256 end;   address owner;
+  mapping(address=>uint256) investments ;
+  constructor() public{
+    end=now+60 days ;
+    owner=msg.sender ;
+ }
+  function invest() public payable{
+    require(phase==0 && raised<goal);
+    investments[msg.sender]+=msg.value ;
+    raised+=msg.value ;
+  }
+  function setPhase(uint256 newPhase) public {
+    require (
+    (newPhase==1 && raised>=goal) ||
+    (newPhase==2 && raised<goal && now>end));
+    phase=newPhase ;
+  }
+  function setOwner(address newOwner) public {      
+    owner=newOwner ;
+  }
+  function withdraw() public {
+    require(phase==1);
+    owner.transfer(raised);
+  }
+  function refund() public {
+    require(phase==2);
+    msg.sender.transfer(investments[msg.sender]);
+    investments[msg.sender]=0;
+  }
+}
