83f46290cc36105236a5a659d7a4e5abf7d8b0c8
contractAnalysis <weisally35@gmail.com>
2024-08-28 16:02:36 -0500

    consider all R and update contract data and configuration

--- rl/config.py
+++ rl/config.py
@@ -26,10 +26,12 @@ NUM_episode=5
 max_svar_value=5740
 max_func_value_element= 70
 
+# recent: after August 19th, 2024
+# consider all Reads and Writes
 rl_parameters={
     "sGuard": {
-        "contract_data_for_env_construction_json_file_name": "rl_sGuard_contracts_data_for_env_construction_5_15_2024_16_in_integer.json",
-        "contract_rw_data_json_file_name": 'rl_sGuard_contracts_data_for_env_construction_5_15_2024_16.json',
+        "contract_data_for_env_construction_json_file_name": "rl_sGuard_contracts_data_for_env_construction_8_19_2024_16_in_integer.json",
+        "contract_rw_data_json_file_name": 'rl_sGuard_contracts_data_for_env_construction_8_19_2024_16.json',
         "dataset": "sGuard",
         "NUM_state_var": 16,
         "NUM_actions": 51,
@@ -37,8 +39,8 @@ rl_parameters={
         "max_func_value_element": 70,
         "ENV_NAME": "ContractEnv_55",
         "NUM_episode": 5,
-        "model_folder": 'ContractEnv_55_model7_sGuard_16_1723696146',
-        "model_file_name_prefix": "105650000",
+        "model_folder": 'ContractEnv_55_model7_sGuard_16_1724508857',
+        "model_file_name_prefix": "167290000",
         "flag_model": 7,
         "goal_indicator": 2,
         "flag_maskable": False,
@@ -47,8 +49,8 @@ rl_parameters={
     },
 
     "small_dataset":{
-        "contract_data_for_env_construction_json_file_name":"rl_small_dataset_contracts_data_for_env_construction_7_4_2024_8_in_integer.json",
-        "contract_rw_data_json_file_name":'rl_small_dataset_contracts_data_for_env_construction_7_4_2024_8.json',
+        "contract_data_for_env_construction_json_file_name":"rl_small_dataset_contracts_data_for_env_construction_8_19_2024_8_in_integer.json",
+        "contract_rw_data_json_file_name":'rl_small_dataset_contracts_data_for_env_construction_8_19_2024_8.json',
         "dataset":"small_dataset",
         "NUM_state_var":8,
         "NUM_actions":20,
@@ -56,8 +58,8 @@ rl_parameters={
         "max_func_value_element":30,
         "ENV_NAME":"ContractEnv_55",
         "NUM_episode":5,
-        "model_folder":'ContractEnv_55_model7_small_dataset_8_1723673782',
-        "model_file_name_prefix":"10300000",
+        "model_folder":'ContractEnv_55_model7_small_dataset_8_1724161007',
+        "model_file_name_prefix":"43100000",
         "flag_model":7,
         "goal_indicator":2,
         "flag_maskable":False,
@@ -65,8 +67,8 @@ rl_parameters={
         "flag_model_whole":True,
     },
     "HoloToken": {
-        "contract_data_for_env_construction_json_file_name": "rl_small_dataset_contracts_data_for_env_construction_7_4_2024_8_in_integer.json",
-        "contract_rw_data_json_file_name": 'rl_small_dataset_contracts_data_for_env_construction_7_4_2024_8.json',
+        "contract_data_for_env_construction_json_file_name":"rl_small_dataset_contracts_data_for_env_construction_8_19_2024_8_in_integer.json",
+        "contract_rw_data_json_file_name":'rl_small_dataset_contracts_data_for_env_construction_8_19_2024_8.json',
         "dataset": "small_dataset",
         "NUM_state_var": 8,
         "NUM_actions": 20,
@@ -74,8 +76,8 @@ rl_parameters={
         "max_func_value_element": 30,
         "ENV_NAME": "ContractEnv_55",
         "NUM_episode": 5,
-        "model_folder": 'ContractEnv_55_model7_small_dataset_8_20_1723664007_HoloToken',
-        "model_file_name_prefix": "4500000",
+        "model_folder": 'ContractEnv_55_model7_small_dataset_8_20_1724125326_HoloToken',
+        "model_file_name_prefix": "17380000",
         "flag_model": 7,
         "goal_indicator": 2,
         "flag_maskable": False,
@@ -85,6 +87,66 @@ rl_parameters={
 
 }
 
+# # old
+# rl_parameters={
+#     "sGuard": {
+#         "contract_data_for_env_construction_json_file_name": "rl_sGuard_contracts_data_for_env_construction_5_15_2024_16_in_integer.json",
+#         "contract_rw_data_json_file_name": 'rl_sGuard_contracts_data_for_env_construction_5_15_2024_16.json',
+#         "dataset": "sGuard",
+#         "NUM_state_var": 16,
+#         "NUM_actions": 51,
+#         "max_svar_value": 5740,
+#         "max_func_value_element": 70,
+#         "ENV_NAME": "ContractEnv_55",
+#         "NUM_episode": 5,
+#         "model_folder": 'ContractEnv_55_model7_sGuard_16_1723696146',
+#         "model_file_name_prefix": "105650000",
+#         "flag_model": 7,
+#         "goal_indicator": 2,
+#         "flag_maskable": False,
+#         "mode": 'predict',
+#         "flag_model_whole": True,
+#     },
+#
+#     "small_dataset":{
+#         "contract_data_for_env_construction_json_file_name":"rl_small_dataset_contracts_data_for_env_construction_7_4_2024_8_in_integer.json",
+#         "contract_rw_data_json_file_name":'rl_small_dataset_contracts_data_for_env_construction_7_4_2024_8.json',
+#         "dataset":"small_dataset",
+#         "NUM_state_var":8,
+#         "NUM_actions":20,
+#         "max_svar_value":80,
+#         "max_func_value_element":30,
+#         "ENV_NAME":"ContractEnv_55",
+#         "NUM_episode":5,
+#         "model_folder":'ContractEnv_55_model7_small_dataset_8_1723673782',
+#         "model_file_name_prefix":"10300000",
+#         "flag_model":7,
+#         "goal_indicator":2,
+#         "flag_maskable":False,
+#         "mode":'predict',
+#         "flag_model_whole":True,
+#     },
+#     "HoloToken": {
+#         "contract_data_for_env_construction_json_file_name": "rl_small_dataset_contracts_data_for_env_construction_7_4_2024_8_in_integer.json",
+#         "contract_rw_data_json_file_name": 'rl_small_dataset_contracts_data_for_env_construction_7_4_2024_8.json',
+#         "dataset": "small_dataset",
+#         "NUM_state_var": 8,
+#         "NUM_actions": 20,
+#         "max_svar_value": 80,
+#         "max_func_value_element": 30,
+#         "ENV_NAME": "ContractEnv_55",
+#         "NUM_episode": 5,
+#         "model_folder": 'ContractEnv_55_model7_small_dataset_8_20_1723664007_HoloToken',
+#         "model_file_name_prefix": "4500000",
+#         "flag_model": 7,
+#         "goal_indicator": 2,
+#         "flag_maskable": False,
+#         "mode": 'predict',
+#         "flag_model_whole": True,
+#     }
+#
+# }
+#
 
 rl_cur_parameters={
         "contract_data_for_env_construction_json_file_name":None,
